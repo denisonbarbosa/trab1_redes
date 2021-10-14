729396cffb4952c4222dd11691d173764ba2d1f2
denisonbarbosa <denisonmb97@gmail.com>
2021-10-14 17:01:27 -0400

    Initial implementation of the methods

--- Message.py
+++ Message.py
@@ -1,3 +1,6 @@
-class Message:
-    def __init__(self, source, destination) -> None:
-        pass
+class Message(object):
+    def __init__(self, args) -> None:
+        self.source = args[0]
+        self.destination = args[1]
+        self.type = args[2]
+        self.content = args[3]
--- init.py
+++
@@ -1,6 +0,0 @@
-def init():
-    pass
-
-
-if __name__ == '__main__':
-    init()
